e938ffe9b2f7695a398d695f4a799e0e1c71eec7
unknown <milena20182018@gmail.com>
2024-06-10 22:01:42 -0300

    feat: Configurar o Sequelize e sincronizar o banco de dados

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const sequelize = require('./config/database');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
@@ -7,6 +8,17 @@ app.use(express.json());
 app.use('/', userRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+const startServer = async () => {
+    try {
+        await sequelize.sync();
+        console.log('Database connected and synchronized');
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+
+startServer();
